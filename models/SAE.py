c230f734f4ea59c15ed062436a996769fca2ca32
GabrielManhaes <43486121+GabrielManhaes@users.noreply.github.com>
2022-12-14 20:03:22 -0300

    Create SAE.py

---
+++ models/SAE.py
@@ -0,0 +1,94 @@
+import tensorflow as tf
+from tensorflow import keras
+
+class Encoder(tf.keras.layers.Layer):
+    """Implementação de Encoder com layers parametrizáveis"""
+
+    def __init__(
+      self,
+      neurons,
+      latent_dim,
+      hidden_activation,
+      name = "encoder",
+      **kwargs
+    ):
+      super(Encoder, self).__init__(name=name, **kwargs)
+      self.neurons = neurons
+      self.latent_dim = latent_dim
+      self.hidden_layers = [Dense(neuron, activation=hidden_activation) for neuron in self.neurons]
+      self.dense_latent = Dense(self.latent_dim)
+
+    def call(self, inputs):
+      x = self.hidden_layers[0](inputs)
+      for layer in self.hidden_layers[1:]:
+        x = layer(x)
+      return self.dense_latent(x)
+
+
+class Decoder(tf.keras.layers.Layer):
+    """Implementação de Decoder com layers parametrizáveis"""
+
+
+    def __init__(
+      self,
+      n_features,
+      neurons,
+      hidden_activation,
+      output_activation,
+      name = "decoder",
+      **kwargs
+    ):
+      super(Decoder, self).__init__(name=name, **kwargs)
+      self.neurons = neurons
+      self.n_features = n_features
+      self.hidden_layers = [Dense(neuron, activation=hidden_activation) for neuron in self.neurons]
+      self.dense_output = Dense(self.n_features, activation=output_activation)
+
+    def call(self, inputs):
+      x = self.hidden_layers[0](inputs)
+      for layer in self.hidden_layers[1:]:
+        x = layer(x)
+      return self.dense_output(x)
+
+class SAE(tf.keras.Model):
+  """ Stacked Autoencoders """
+
+  def __init__(
+    self,
+    n_features,
+    encoder_neurons,
+    decoder_neurons,
+    latent_dim,
+    hidden_activation,
+    output_activation,
+    n_stacks,
+    name = 'SAE',
+    **kwargs
+  ):
+    super(SAE, self).__init__(name=name, **kwargs)
+    self.n_features = n_features
+    self.encoder_neurons = encoder_neurons
+    self.decoder_neurons = decoder_neurons
+    self.latent_dim = latent_dim
+    self.hidden_activation = hidden_activation
+    self.output_activation = output_activation
+    self.n_stacks = n_stacks
+    self.encoder = Encoder(
+      latent_dim = self.latent_dim,
+      neurons = self.encoder_neurons,
+      hidden_activation = self.hidden_activation
+    )
+    self.decoder = Decoder(
+      n_features = self.n_features,
+      neurons = self.decoder_neurons,
+      hidden_activation = self.hidden_activation,
+      output_activation = self.output_activation
+    )
+
+  def call(self, inputs):
+    x = self.encoder(inputs)
+    for _ in range(self.n_stacks - 1):
+        x = self.decoder(x)
+        x = self.encoder(x)
+    reconstructed = self.decoder(x)
+    return reconstructed
